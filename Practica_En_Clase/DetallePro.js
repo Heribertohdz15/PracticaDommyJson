f85ec3d57ded77931b16fb34636465d012b09ae5
Heribertohdz15 <heribertobta.z@gmail.com>
2026-01-31 23:52:52 -0600

    Practica Dommyjson

---
+++ Practica_En_Clase/DetallePro.js
@@ -0,0 +1,44 @@
+const paramentros = new URLSearchParams(window.location.search);
+const idProducto = paramentros.get("id");
+
+const cargarDetalle = async () => {
+    try {
+        const respuesta = await fetch(`https://dummyjson.com/products/${idProducto}`);
+        const producto = await respuesta.json();
+        console.log("Datos recibidos:", producto); 
+        Detalle_producto(producto);
+    } catch (error) {
+        console.error("Error al cargar los productos:", error);
+        alert("Hubo un error al cargar los datos.");
+    }
+};
+
+const Detalle_producto = (producto) => {
+    const contenedor = document.getElementById("detalleProducto");
+    
+    // El orden solicitado: Nombre, Imagen, Descripcion, Precio, Marca, Categoria
+    contenedor.innerHTML = `    
+        <div class="detalle-contenedor"> 
+            <h3 class="practice-title">${producto.title}</h3>
+            
+            <div class="detalle-visual">
+                <img src="${producto.thumbnail}" alt="${producto.title}">
+            </div>
+            
+            <div class="detalle-info">
+                <div class="meta-datos">
+                    <strong>Descripcion: </strong>${producto.description}</p> 
+                    <p><strong>Precio: </strong> $${producto.price}</p>
+                    <p><strong>Marca: </strong> ${producto.brand}</p>
+                    <p><strong>Categoría: </strong> ${producto.category}</p>
+                    <p><strong>Stock: </strong> ${producto.stock} unidades</p>
+                    <p><strong>Rating: </strong> ${producto.rating}</p>
+                </div>
+                
+                <button type="button" class="btn-regresar" onclick="window.history.back()">Regresar</button>
+            </div>
+        </div>
+    `;
+}
+
+cargarDetalle();
